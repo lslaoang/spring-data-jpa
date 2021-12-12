67bed434c178983e088fc864d0bb13d3f705385e
Laurel Lao Ang <laurellaoang@gmail.com>
2021-12-12 13:28:57 +0800

    Updated the viewAllBook GetMapping

--- src/main/java/io/lao/alloutjpa/controller/BookController.java
+++ src/main/java/io/lao/alloutjpa/controller/BookController.java
@@ -1,7 +1,6 @@
 package io.lao.alloutjpa.controller;
 
 import io.lao.alloutjpa.dao.Genre;
-import io.lao.alloutjpa.dao.MgaAklat;
 import io.lao.alloutjpa.model.Books;
 import io.lao.alloutjpa.service.bookservice.BookService;
 import io.lao.alloutjpa.service.viewbookservice.BookViewService;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -32,14 +30,15 @@ public class BookController {
     }
 
     @GetMapping
-    public List<BookView> findAll(){
-        MgaAklat mgaAklat = new MgaAklat();
-        mgaAklat.setMgaAklat(bookService.getAllAklat());
-        final List<BookView> bookViewList  = new ArrayList<>();
-        mgaAklat.getMgaAklat().forEach(aklat ->
-                bookViewList.add(new BookView(aklat.getId(),aklat.getName(), aklat.getGenre())));
-
-        return bookViewList;
+    public ResponseEntity<?> viewAllBook(){
+        List<BookView> bookViewList;
+        try{
+            bookViewList =   bookViewService.viewAllBook();
+        }catch (NullPointerException e){
+            LOGGER.warn("No record detected.");
+            return  new ResponseEntity<>("Something went wrong",HttpStatus.NO_CONTENT);
+        }
+        return  new ResponseEntity<>(bookViewList,HttpStatus.OK);
     }
 
     @GetMapping(value = "/{bookId}")
@@ -50,9 +49,9 @@ public class BookController {
             bookView = bookViewService.viewBookById(bookId);
         } catch (NullPointerException e){
             LOGGER.warn("No record of {} detected.",bookId);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>("No record found.",HttpStatus.NO_CONTENT);
         }
-        LOGGER.warn("Fetching record of {} success.",bookId);
+        LOGGER.info("Fetching record of {} success.",bookId);
         return new ResponseEntity<>(bookView, HttpStatus.OK);
     }
 
@@ -65,7 +64,7 @@ public class BookController {
             bookService.convertToAklatAndSave(new Books(bookId,bookName,genre));
             LOGGER.info("Adding new book success.");
         } catch (MethodArgumentTypeMismatchException | NullPointerException e){
-            LOGGER.info("Argument mismatch or invalid book detected.");
+            LOGGER.warn("Argument mismatch or invalid book detected.");
             return new ResponseEntity<>("Something went wrong.",HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>("Saving new book record success!",HttpStatus.OK);
@@ -79,7 +78,7 @@ public class BookController {
             bookService.convertToAklatAndSave(books);
             LOGGER.info("Adding new book success.");
          } catch (MethodArgumentTypeMismatchException | NullPointerException e){
-            LOGGER.info("Argument mismatch or invalid book detected.");
+            LOGGER.warn("Argument mismatch or invalid book detected.");
             return new ResponseEntity<>("Something went wrong.",HttpStatus.BAD_REQUEST);
          }
         return new ResponseEntity<>("Saving new book record success!",HttpStatus.ACCEPTED);
