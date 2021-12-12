10642df813b16167f944b009ba9a4f9563eaecdb
Laurel Lao Ang <laurellaoang@gmail.com>
2021-12-12 13:19:40 +0800

    Additional Validation and response created in Controller

--- src/main/java/io/lao/alloutjpa/controller/BookController.java
+++ src/main/java/io/lao/alloutjpa/controller/BookController.java
@@ -1,21 +1,27 @@
 package io.lao.alloutjpa.controller;
 
-import io.lao.alloutjpa.dao.Aklat;
 import io.lao.alloutjpa.dao.Genre;
 import io.lao.alloutjpa.dao.MgaAklat;
+import io.lao.alloutjpa.model.Books;
 import io.lao.alloutjpa.service.bookservice.BookService;
 import io.lao.alloutjpa.service.viewbookservice.BookViewService;
 import io.lao.alloutjpa.view.BookView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/books", method = {RequestMethod.GET,RequestMethod.POST})
 public class BookController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BookController.class);
 
     final BookService bookService;
     final BookViewService bookViewService;
@@ -37,50 +43,47 @@ public class BookController {
     }
 
     @GetMapping(value = "/{bookId}")
-    public ResponseEntity<BookView> getBookById(@PathVariable("bookId") int bookId){
-        return new ResponseEntity<>(bookViewService.viewBookById(bookId), HttpStatus.OK);
-    }
+    public ResponseEntity<?> getBookById(@PathVariable("bookId") int bookId){
 
-    @RequestMapping(value = "/add")
-    public List<BookView> addBookByParameter(@RequestParam(value = "bookId") int bookId,
-                          @RequestParam(value = "bookName") String bookName,
-                          @RequestParam(value = "genre") String genre){  //Change to string for test
-
-
-        //create this validation
-        Genre thisGenre = validateGenre(genre);
-
-        bookService.saveBook(new Aklat(bookId,bookName,thisGenre));
-        List<BookView> listOfBooks = bookViewService.viewAllBook();
-        return listOfBooks;
-    }
-
-    private static Genre validateGenre(String strGenre){
-        Genre thisGenre = null;
+        BookView bookView;
         try{
-            thisGenre = Genre.valueOf(strGenre);
-            return thisGenre;
-        }catch (IllegalArgumentException e){
-            thisGenre = Genre.UNDEFINED;
-            System.out.println("Genre unknown, setting to default genre.");
-            return thisGenre;
+            bookView = bookViewService.viewBookById(bookId);
+        } catch (NullPointerException e){
+            LOGGER.warn("No record of {} detected.",bookId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
+        LOGGER.warn("Fetching record of {} success.",bookId);
+        return new ResponseEntity<>(bookView, HttpStatus.OK);
     }
+
+    @PostMapping(value = "/add")
+    public ResponseEntity<?>  addBookByParameter(@RequestParam(value = "bookId") @Valid int bookId,
+                          @RequestParam(value = "bookName") String bookName,
+                          @RequestParam(value = "genre") @Valid Genre genre){
+
+        try{
+            bookService.convertToAklatAndSave(new Books(bookId,bookName,genre));
+            LOGGER.info("Adding new book success.");
+        } catch (MethodArgumentTypeMismatchException | NullPointerException e){
+            LOGGER.info("Argument mismatch or invalid book detected.");
+            return new ResponseEntity<>("Something went wrong.",HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("Saving new book record success!",HttpStatus.OK);
+    }
+
+
+    @PostMapping(value = "/add/v2")
+    public ResponseEntity<?> addBookByObject(@RequestBody Books books){
+
+        try{
+            bookService.convertToAklatAndSave(books);
+            LOGGER.info("Adding new book success.");
+         } catch (MethodArgumentTypeMismatchException | NullPointerException e){
+            LOGGER.info("Argument mismatch or invalid book detected.");
+            return new ResponseEntity<>("Something went wrong.",HttpStatus.BAD_REQUEST);
+         }
+        return new ResponseEntity<>("Saving new book record success!",HttpStatus.ACCEPTED);
+    }
+
     //TODO: Implement ModelView aspect
-    /*
-    @RequestMapping(value = {"/addbookTest","addbooktest.html"}, method = RequestMethod.GET)
-    @ResponseBody
-    public ModelAndView returnThis(){
-        return new ModelAndView("addBook.html");
-    }
-
-    @RequestMapping(value = "/addbook")
-    public String addBook(@ModelAttribute("Books")Books books){
-
-       // bookService.convertToBookAndSave(books);
-        // List<BookView> listOfBooks = bookViewService.viewAllBook();
-        return "Success";
-    }
-
-     */
 }
