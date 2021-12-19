d0eccdce3da1056d3602a22fb16378cecaf037ec
Laurel Lao Ang <laurellaoang@gmail.com>
2021-12-19 12:05:01 +0800

    Updated Repository with new ID data type

--- src/main/java/io/lao/alloutjpa/repository/StudentRepository.java
+++ src/main/java/io/lao/alloutjpa/repository/StudentRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends JpaRepository<JpaStudent, Integer> {
+public interface StudentRepository extends JpaRepository<JpaStudent, String> {
 }
