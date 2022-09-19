789fcf4e97a34db1d02cb9cb628ca40c10d7a909
Laurel Lao Ang <laurellaoang@gmail.com>
2022-09-19 08:40:47 +0800

    remove builder from models

--- src/main/java/io/lao/alloutjpa/model/Book.java
+++ src/main/java/io/lao/alloutjpa/model/Book.java
@@ -1,14 +1,12 @@
 package io.lao.alloutjpa.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class Book {
 
     private String id;
--- src/main/java/io/lao/alloutjpa/model/Student.java
+++ src/main/java/io/lao/alloutjpa/model/Student.java
@@ -1,10 +1,8 @@
 package io.lao.alloutjpa.model;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class Student {
 
     private String id;
